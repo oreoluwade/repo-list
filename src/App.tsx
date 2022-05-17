18f1cc2fa8a7c3ff1a6fb512a290c6cd0203f52b
Oreoluwa Aboluwarin <oreoluwade@gmail.com>
2022-05-17 11:03:33 +0100

    Create search bar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import SearchBar from "./components/search-bar";
 
-function App() {
+const App: React.FC = () => {
+  const [searchQuery, setSearchQuery] = useState("re");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen flex justify-center items-center p-10">
+      <div className="flex flex-col h-full w-full">
+        <SearchBar
+          value={searchQuery}
+          onChange={e => {
+            setSearchQuery(e.target.value);
+          }}
+          placeholder="Start typing to search for repositories"
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/search-bar.tsx
@@ -0,0 +1,32 @@
+import { ReactComponent as SearchIcon } from "../svgs/search-icon.svg";
+
+interface SearchBoxProps {
+  value: string;
+  onChange: (arg: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: () => void;
+  placeholder?: string;
+}
+
+const SearchBar: React.FC<SearchBoxProps> = ({
+  value,
+  onChange = () => {},
+  onBlur = () => {},
+  placeholder
+}) => {
+  return (
+    <div className="w-1/2 border border-[2px] border-solid border-[grey] h-[50px] rounded-lg flex items-center mx-auto px-4 py-[2px]">
+      <SearchIcon className="cursor-pointer" />
+      <div className="w-2" />
+      <input
+        type="search"
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        placeholder={placeholder}
+        className="grow px-3 text-xs border-hidden h-full outline-none"
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
